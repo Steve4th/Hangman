784062e83d73d194b1d51e88653abbae703cb090
Steve 4th <steveforth@hotmail.com>
2015-05-22 16:25:18 +0100

    Added Game view and controller

---
+++ Hangman/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Hangman.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult PlayGame()
+        {
+            return View();
+        }
+    }
+}

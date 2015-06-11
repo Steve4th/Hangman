1f1843536f601d49d7d3bfdea52b6c8ad38b7fd5
Steve 4th <steveforth@hotmail.com>
2015-06-11 09:22:46 +0100

    Include JS scripts generated from typescript in source control

---
+++ Hangman/Scripts/game/Game.js
@@ -0,0 +1,9 @@
+var game = (function () {
+    function game() {
+    }
+    game.prototype.reset = function (newWord) {
+        this.activeWord = newWord;
+    };
+    return game;
+})();
+//# sourceMappingURL=game.js.map
---
+++ Hangman/Scripts/game/game.tests.js
@@ -0,0 +1,9 @@
+/// <reference path="../typings/jasmine/jasmine.d.ts" />
+describe("game", function () {
+    it("Should display underscore for each letter in word for a new game", function () {
+        var g = new game();
+        g.reset("hangman");
+        expect(g.activeWord).toEqual("_______");
+    });
+});
+//# sourceMappingURL=game.tests.js.map

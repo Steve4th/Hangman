ce754d5a01e6c0c31aaa0f70b0707a99ee215a4d
Steve 4th <steveforth@hotmail.com>
2015-07-13 12:08:22 +0100

    Added inital tests for getting phrases from the web api service

---
+++ Hangman.Tests/Api/PhraseGetTests.cs
@@ -0,0 +1,32 @@
+﻿using Hangman.Areas.Api.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Tests.Api
+{
+    [TestClass]
+    public class PhraseGetTests
+    {
+        [TestMethod]
+        public void GetPhrase_NoParameters_ExpectPhraseTermReturned()
+        {
+            var apiController = new PhraseController();
+            string response = apiController.Get();
+            Console.WriteLine(response);
+            Assert.IsFalse(string.IsNullOrEmpty(response));
+        }
+
+        [TestMethod]
+        public void GetPhrase_IdSpecified_ExpectPhraseTermReturned()
+        {
+            var apiController = new PhraseController();
+            string response = apiController.Get(1);
+            Console.WriteLine(response);
+            Assert.IsFalse(string.IsNullOrEmpty(response));            
+        }
+    }
+}

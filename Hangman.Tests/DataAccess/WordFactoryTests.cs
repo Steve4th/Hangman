ca7f128b8c23344b58f76dae1910b48e26143096
Steve 4th <steveforth@hotmail.com>
2015-08-11 10:42:02 +0100

    Added WordFactory with failing tests Included Moq package in prep for getting the tests to work

---
+++ Hangman.Tests/DataAccess/WordFactoryTests.cs
@@ -0,0 +1,54 @@
+﻿using Hangman.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Tests.DataAccess
+{
+    [TestClass]
+    public class WordFactoryTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void GetWord_EmptyList_ExpectException()
+        {
+            IWordListReader reader = new WordListFileReader();
+            var factory = new WordFactory(reader);
+            var word = factory.GetWord();
+        }
+
+        [TestMethod]
+        public void GetWord_OneItemList_ExpectSameWordReturnedOnEachCall()
+        {
+            const string expectedWord = "WordToReturnPlease";
+
+            IWordListReader reader = new WordListFileReader();
+            var factory = new WordFactory(reader);
+
+            var word = factory.GetWord();
+
+            Assert.AreEqual(expectedWord, word);
+        }
+
+        [TestMethod]
+        public void GetWord_MultiItemList_ExpectDifferentWordsReturnedOnSeperateCalls()
+        {
+            IWordListReader reader = new WordListFileReader();
+            var factory = new WordFactory(reader);
+
+            var listOfWordsReturned = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                var word = factory.GetWord();
+                listOfWordsReturned.Add(word);
+            }
+
+            var wordCounts = listOfWordsReturned.GroupBy(w => w);
+
+            Assert.IsTrue(wordCounts.Count() > 1);
+        }
+    }
+}
---
+++ Hangman/DataAccess/WordFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Hangman.DataAccess
+{
+    public class WordFactory
+    {
+        IWordListReader _wordListReader;
+
+        public WordFactory(IWordListReader wordReader)
+        {
+            _wordListReader = wordReader;
+        }
+
+        public string GetWord()
+        {
+            var words = _wordListReader.GetAllWords();
+            var wordToReturn = words.First();
+
+            return wordToReturn;
+        }
+    }
+}

7392c6644a8ba9c0e417da8a2dfae0f47a7de5fe
Steve 4th <steveforth@hotmail.com>
2015-08-11 14:25:45 +0100

    added gulp dependances to hangular project using npm

---
+++ Hangular/gulpfile.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="typings/gulp/gulp.d.ts""/>
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('validate-js', function () {
+    return gulp.src([
+		'./scripts/**/*.js',
+		'./*.js'
+    ])
+	.pipe(jscs())
+	.pipe(jshint())
+	.pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+});

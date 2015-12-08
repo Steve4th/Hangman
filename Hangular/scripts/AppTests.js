eca23655494c01297ae13c3a142a186312d05111
Steve 4th <steveforth@hotmail.com>
2015-12-08 13:06:21 +0000

    Added client tests back into module

--- Hangular/scripts/AppTests.js
+++ Hangular/scripts/AppTests.js
@@ -1,12 +1,16 @@
 /// <reference path="../typings/jasmine/jasmine.d.ts" />
-//module Hangular.Tests {
-describe("First App Tests", function () {
-    it("Should pass a test", function () {
-        expect(1).toEqual(1);
-    });
-    it("Should Fail", function () {
-        expect(2).toEqual(1);
-    });
-});
-//} 
+var Hangular;
+(function (Hangular) {
+    var Tests;
+    (function (Tests) {
+        describe("First App Tests", function () {
+            it("Should pass a test", function () {
+                expect(1).toEqual(1);
+            });
+            it("Should Fail", function () {
+                expect(2).toEqual(1);
+            });
+        });
+    })(Tests = Hangular.Tests || (Hangular.Tests = {}));
+})(Hangular || (Hangular = {}));
 //# sourceMappingURL=AppTests.js.map
